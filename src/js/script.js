f6f343cd9de50124eee52b137359c1d9186ac17a
vada25 <vada1981@gmail.com>
2024-09-06 12:32:08 +0300

    upd

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    // hamburger
+
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+    const overlay = document.getElementById('overlay');
+    
+    const body = document.body;
+    
+            // open and close menu 
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('open');
+        overlay.classList.toggle('active');
+        body.classList.toggle('body-no-scroll');
+    });
+            // close menu on overlay click
+    overlay.addEventListener('click', () => {
+        navLinks.classList.remove('open');
+        overlay.classList.remove('active');
+        body.classList.remove('body-no-scroll');
+    });
+   
+
     //carousel Three
 
     function carouselThree() {
@@ -17,4 +39,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     window.addEventListener('resize', carouselThree);
     carouselThree();
+
+    
 });
